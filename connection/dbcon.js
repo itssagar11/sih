2fddde3e0886ce4c65553646ea531e9c879082ef
shauryaguleria007 <shauryaguleria12345678@gmail.com>
2022-07-25 19:04:21 +0530

    creater dbconn

---
+++ connection/dbcon.js
@@ -0,0 +1,12 @@
+const mySql = require("mysql2");
+
+const pool = mySql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD
+});
+
+module.exports = {
+    pool
+}
